14613c0309b728aff4e2c71ddc5bc4b982f5e396
Benjamin Tenision <benjamin4441111@gmail.com>
2026-06-28 10:24:46 +0530

    Fix PostgreSQL URL

--- 01_app.py
+++ 01_app.py
@@ -23,7 +23,10 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key_here')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
+database_url = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
